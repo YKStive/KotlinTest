188c20021640fff53a2b50f96dba0ec3e7199aa7
1551332434@qq.com <youke@youloft.com>
2020-04-16 22:35:52 +0800

    "关闭硬件加速"

--- app/src/main/java/com/youloft/kotlintest/widget/CircleView.kt
+++ app/src/main/java/com/youloft/kotlintest/widget/CircleView.kt
@@ -2,7 +2,9 @@ package com.youloft.kotlintest.widget
 
 import android.content.Context
 import android.graphics.*
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.youloft.kotlintest.R
 
@@ -13,11 +15,13 @@ import com.youloft.kotlintest.R
  * @desc
  */
 class CircleView : ConstraintLayout {
+    private var drawable: Drawable?
+    private var mHeight: Int = 0
     private var mCircleCount: Int = 0
     private var mCircleWith: Int = 0
     private var mWith: Int = 0
-    private var mCircleLeft: Float = 20f
-    private var mTop: Float = 15f
+    private var mCircleLeft: Float = 0f
+    private var mTop: Float = 0f
     private var mCircle: Bitmap
     private var mPaint: Paint
 
@@ -37,6 +41,9 @@ class CircleView : ConstraintLayout {
         mCircle = BitmapFactory.decodeResource(context.resources, R.drawable.ic_circle)
         mCircleWith = mCircle.width
 
+         drawable = resources.getDrawable(R.drawable.ic_circle, null)
+
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
 
 
     }
@@ -44,18 +51,28 @@ class CircleView : ConstraintLayout {
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         mWith = measuredWidth
+        mHeight = measuredHeight
+
 
         mCircleCount = mWith / mCircleWith
 
     }
 
     override fun dispatchDraw(canvas: Canvas) {
+//        val saveLayerCount = canvas.saveLayer(0f, 0f, mWith.toFloat(), mHeight.toFloat(), mPaint)
         super.dispatchDraw(canvas);
-        mPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.MULTIPLY)
+
+
+
+//        drawable?.draw(canvas)
+
+        mPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
         for (index in 1..mCircleCount) {
             canvas.drawBitmap(mCircle, mCircleLeft, mTop, mPaint)
             mCircleLeft += mCircleWith
         }
+//        mPaint.xfermode = null
+//        canvas.restoreToCount(saveLayerCount)
     }
 
 }
